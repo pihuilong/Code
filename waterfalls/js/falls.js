944a4455c6d78fe3769648d72f30062bc310b5e5
pihuilong <pihuilong@qq.com>
2018-01-16 17:25:18 +0800

    continue

--- waterfalls/js/falls.js
+++ waterfalls/js/falls.js
@@ -1,93 +1,127 @@
 var falls = {
     //每列当前的高度
-  topArry:[0,0,0,0,0],
-  //每列当前距离浏览器的左边距
-  leftArry:[],
-  //每次加载的图片
-  imgArray:[],
-  //每次加载的第一张图片相对于所有图片（包括已加载图片）的索引
-  num:0,
-  //加载图片数，默认 9 张，根据实际修改
-  dataNum:9,
+    topArry: [],
+    //每列当前距离浏览器的左边距
+    leftArry: [],
+    //每次加载的图片
+    imgArray: [],
+    //每次加载的第一张图片相对于所有图片（包括已加载图片）的索引
+    num: 0,
+    //加载图片数
+    dataNum: 0,
+    //图片列数
+    cols: 0,
 }
 
 //初始化函数
-$(function(){
-  getData();
-//   getMinHeight();
-  window.onscroll = function(){
-      var minHeight = getMinHeight();
-      //当屏幕滚动的距离大于最短高度的二分之一（可任意设置，看个人喜好），再次加载图片
-        if(window.scrollY>minHeight/2){
-            getData();
-        }
-  }
+$(function () {
+    start();
+    // $(window).resize(function(){
+    //     $("#container").html("");
+    //     start();
+    // });
 });
 
-function getData(){
-    $.getJSON("json/falls.json",function(data){
-        if(data.success){
-            var row = data.row,
-                  len = row.length,
-                  html = "";
-            
-            //将获取到的数据通过字符串连接的方式添加到 html 内容中
-            for(var i=0;i<len;i++){
-                html+="<div class='img-box'><img src='"+row[i].src+"'/></div>";
-            }
-            $("#container").append(html);
-           
-
-            //修改每张图片的位置
-            reset();
+function start(){
+    init();
+    run();
+    window.onscroll = function () {
+        var minHeight = getMinHeight();
+        if (checkWillLoad()) {
+            run();
         }
-    })
-}
-
-function getMinHeight(){
-    var minHeight = Math.min.apply(null,falls.topArry);
-    return minHeight;
-}
-
-function getLeft(){
-    for(var i=0;i<5;i++){
-        var left = $("#container .img-box").eq(i).offset().left;
-        falls.leftArry.push(left);
     }
 }
 
-function reset(){
-    getLeft();
-    //可确保不同的显示器屏幕能初始化时便将图片布满屏幕
-   if(getMinHeight()<window.screen.height){
-       getData();
-   }
-   falls.imgArray = [];
-   $("#container .img-box").each(function(i){
-       if(i >= falls.num){
-           falls.imgArray.push(this);
-       }
-   });
-   falls.num += falls.dataNum;
+//使用JSON数据模拟从后台获取图片数据
+function getData() {
+    return $.getJSON("json/falls.json", function (data) {    //此处返回一个jquery的promise对象
+        if (data.success) {
+            var row = data.row,
+                html = "",
+                len = row.length;
+            falls.dataNum += len;
 
+            //将获取到的数据通过字符串连接的方式添加到 html 内容中
+            for (var i = 0; i < len; i++) {
+                html += "<div class='img-box'><img src='" + row[i].src + "'/></div>";
+            }
+            $("#container").append(html);
+        }
+    });
+}
+
+//初始化函数（主要初始化：列数，每列左偏移，每列高度置为0）
+function init() {
+    getData().then(function () {
+        var $boxs = $("#container .img-box");
+        falls.cols = Math.floor($("#container").width() / $boxs.eq(0).width());
+        for (var i = 0; i < falls.cols; i++) {
+            var left = $boxs.eq(i).offset().left;
+            falls.leftArry.push(left);
+            falls.topArry[i] = 0;
+        }
+    });
+}
+
+//主要运行函数
+function run() {
+    getData().then(function () {
+        //修改每张图片的位置
+        reset();
+    });
+}
+
+//获取所有列中的最小高度列
+function getMinHeight() {
+    var minHeight = Math.min.apply(null, falls.topArry);
+    return minHeight;
+}
+
+//reset用于对图片进行重新定位
+function reset() {
+    var $boxs = $("#container .img-box");
+    falls.imgArray = [];
+    $boxs.each(function (i) {
+        if (i >= falls.num) {      //这里我们只需要拿到新的没有定位过的图片，不用重新定位所有的，提升性能
+            falls.imgArray.push(this);
+        }
+    });
+    falls.num += falls.dataNum;
     var img = falls.imgArray;
-    for(var i=0,len=img.length;i<len;i++){
+    for (var i = 0, len = img.length; i < len; i++) {
         var minHeight = getMinHeight(),
             index = 0;
-        
-        for(var x=0;x<falls.dataNum;x++){
-            if(minHeight == falls.topArry[x]){
+        //为了取到高度最小列的角标
+        for (var x = 0; x < falls.cols; x++) {
+            if (minHeight == falls.topArry[x]) {
                 index = x;
                 break;
             }
         }
-
+        //重新定位
         $(img[i]).css({
-            "position" : "absolute",
-            "top" : minHeight,
-            "left" : falls.leftArry[index]
+            "position": "absolute",
+            "top": minHeight,
+            "left": falls.leftArry[index]
         });
-
+        //更新最短列
         falls.topArry[index] += $(img[i]).height();
     }
+    falls.dataNum = 0;
+    //可确保不同的显示器屏幕能初始化时便将图片布满屏幕
+    if (getMinHeight() < window.screen.height) {
+        setTimeout(function () {      //确保浏览器已经重排完毕，否则会出现一些图片叠加在一起
+            run();
+        }, 1)
+    }
+}
+
+//判断是否加载图片的条件：最后一张图片顶部相对于页面的位置>=文档高度+滚动条的垂直位置
+function checkWillLoad(){
+    var lastBox = $("#container .img-box").last();
+    var topOffset = $(lastBox).offset().top;
+    var screenHeight = $(window).height();
+    var scrollTop = $(window).scrollTop();
+    return topOffset <= screenHeight + scrollTop;
 }
--- waterfalls/js/waterfall.js
+++ waterfalls/js/waterfall.js
@@ -1,75 +1,128 @@
-function waterFall(){
-    var allBox = $("#container .img-box");
-    var boxWidth = $(allBox).eq(0).outerWidth();
-    var screenWidth = $(window).width();
-    var cols = Math.floor((screenWidth-200)/boxWidth);
+//初始化函数
+$(function () {
+    var waterfall = new falls();
+    waterfall.start();
+    // $(window).resize(function(){
+    //     $("#container").html("");
+    //     start();
+    // });
+});
 
-    $("#container").css({
-        "width" : cols*boxWidth + "px",
-        "margin" : "0 auto",
-    });
-
-    var leftArr = [];
-    var heightArr = [];
-    $.each(allBox,function(index,value){
-        var boxHeight = $(value).outerHeight();
-        var left;
-        if(index<cols){
-            heightArr[index] = boxHeight;
-            left = allBox.eq(index).offset().left;
-            leftArr.push(left);
-        }else{
-            var minBoxHeight = Math.min.apply(null,heightArr);
-            var minBoxIndex = $.inArray(minBoxHeight,heightArr);
-            $(value).css({
-                "position" : "absolute",
-                "top" : minBoxHeight + "px",
-                "left" : leftArr[minBoxIndex] + "px",
-            });
-            heightArr[minBoxIndex] += boxHeight;
-        }
-    })
+var falls = function(){
+    //每列当前的高度
+    this.topArry = [];
+    //每列当前距离浏览器的左边距
+    this.leftArry = [];
+    //每次加载的图片
+    this.imgArray = [];
+    //每次加载的第一张图片相对于所有图片（包括已加载图片）的索引
+    this.num = 0;
+    //加载图片数
+    this.dataNum = 0;
+    //图片列数
+    this.cols = 0;
 }
 
-function checkWillLoad(){
+falls.prototype.start = function(){
+    this.init();
+    this.run();
+    // window.onscroll = function () {
+    //     var minHeight = getMinHeight();
+    //     if (checkWillLoad()) {
+    //         run();
+    //     }
+    // }
+}
+
+//使用JSON数据模拟从后台获取图片数据
+falls.prototype.getData = function() {
+    return $.getJSON("json/falls.json", function (data) {    //此处返回一个jquery的promise对象
+        if (data.success) {
+            var row = data.row,
+                html = "",
+                len = row.length;
+            this.dataNum += len;
+
+            //将获取到的数据通过字符串连接的方式添加到 html 内容中
+            for (var i = 0; i < len; i++) {
+                html += "<div class='img-box'><img src='" + row[i].src + "'/></div>";
+            }
+            $("#container").append(html);
+        }
+    });
+}
+
+//初始化函数（主要初始化：列数，每列左偏移，每列高度置为0）
+falls.prototype.init = function() {
+    this.getData().then(function () {
+        var $boxs = $("#container .img-box");
+        this.cols = Math.floor($("#container").width() / $boxs.eq(0).width());
+        for (var i = 0; i < this.cols; i++) {
+            var left = $boxs.eq(i).offset().left;
+            this.leftArry.push(left);
+            this.topArry[i] = 0;
+        }
+    });
+}
+
+//主要运行函数
+falls.prototype.run = function() {
+    this.getData().then(function () {
+        //修改每张图片的位置
+        this.reset();
+    });
+}
+
+//获取所有列中的最小高度列
+falls.prototype.getMinHeight = function() {
+    var minHeight = Math.min.apply(null, this.topArry);
+    return minHeight;
+}
+
+//reset用于对图片进行重新定位
+falls.prototype.reset = function() {
+    var $boxs = $("#container .img-box");
+    this.imgArray = [];
+    $boxs.each(function (i) {
+        if (i >= this.num) {      //这里我们只需要拿到新的没有定位过的图片，不用重新定位所有的，提升性能
+            this.imgArray.push(this);
+        }
+    });
+    this.num += this.dataNum;
+    var img = this.imgArray;
+    for (var i = 0, len = img.length; i < len; i++) {
+        var minHeight = getMinHeight(),
+            index = 0;
+        //为了取到高度最小列的角标
+        for (var x = 0; x < this.cols; x++) {
+            if (minHeight == this.topArry[x]) {
+                index = x;
+                break;
+            }
+        }
+        //重新定位
+        $(img[i]).css({
+            "position": "absolute",
+            "top": minHeight,
+            "left": this.leftArry[index]
+        });
+        //更新最短列
+        this.topArry[index] += $(img[i]).height();
+    }
+    this.dataNum = 0;
+    //可确保不同的显示器屏幕能初始化时便将图片布满屏幕
+    if (getMinHeight() < window.screen.height) {
+        setTimeout(function () {      //确保浏览器已经重排完毕，否则会出现一些图片叠加在一起
+            run();
+        }, 1)
+    }
+}
+
+//判断是否加载图片的条件：最后一张图片顶部相对于页面的位置>=文档高度+滚动条的垂直位置
+falls.prototype.checkWillLoad = function(){
     var lastBox = $("#container .img-box").last();
     var topOffset = $(lastBox).offset().top;
     var screenHeight = $(window).height();
     var scrollTop = $(window).scrollTop();
     return topOffset <= screenHeight + scrollTop;
 }
-
-function getData(){
-    var data =  [{
-        "src" : "img/0.jpg"
-    },{
-        "src" : "img/1.jpg"
-    },{
-        "src" : "img/2.jpeg"
-    },{
-        "src" : "img/3.jpg"
-    },{
-        "src" : "img/4.jpg"
-    },{
-        "src" : "img/5.jpg"
-    },{
-        "src" : "img/6.jpg"
-    },{
-        "src" : "img/7.jpeg"
-    },{
-        "src" : "img/8.jpg"
-    }]
-    var  len = data.length;
-    var html = "";
-    for(var i=0;i<len;i++){
-        html+="<div class='img-box'><img src='"+data[i].src+"'/></div>";
-    }
-    $("#container").append(html);
-
-    waterFall();
-}
-
-
-$(function(){
-    getData();
-});
